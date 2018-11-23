3f4cc29185ef613e12644c915e616a530262a75c
Ankur Gupta <agupta@mail.uni-paderborn.de>
2018-11-23 17:33:20 +0100

    Methods related to FileUpload and Files retrieval

---
+++ src/main/java/de/tud/cs/stg/zenodo/DepositionFile.java
@@ -0,0 +1,32 @@
+package de.tud.cs.stg.zenodo;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+/**
+ * Created by agupta on 19.11.2018.
+ */
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class DepositionFile {
+
+	public String id;
+	public String filename;
+	public Integer filesize;
+	public String checksum;
+	
+	public Links links;
+	
+	
+	/**
+	 * Created by agupta on 19.11.18.
+	 */
+	 @JsonIgnoreProperties(ignoreUnknown = true)
+	    public static class Links {
+	        public String download;
+	        public String self;
+	    }
+	 
+	 
+}
+
+
---
+++ src/main/java/de/tud/cs/stg/zenodo/FileMetadata.java
@@ -0,0 +1,12 @@
+package de.tud.cs.stg.zenodo;
+
+import java.io.File;
+
+public class FileMetadata {
+
+	public FileMetadata(File f) {
+		this.file =f;
+	}
+	
+	public File file;
+}
