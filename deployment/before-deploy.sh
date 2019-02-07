316ea1f601f96afa44754e94576f82976ce217c8
Ben Hermann <ben.hermann@googlemail.com>
2019-02-07 20:01:34 +0100

    Removed unnecessary scripts

--- deployment/before-deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-    openssl aes-256-cbc -K $encrypted_e5c65c774293_key -iv $encrypted_e5c65c774293_iv -in deployment/signingkey.asc.enc -out deployment/signingkey.asc -d
-    gpg --fast-import deployment/signingkey.asc
-fi
